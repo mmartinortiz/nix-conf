adaba93ecd8f49462b67938ff29e2db1961dc8c4
manolo <mmartinortiz@gmail.com>
2023-07-15 14:54:04 +0200

    debugging

--- flake.nix
+++ flake.nix
@@ -4,21 +4,28 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
 		flake-utils.url = "github:numtide/flake-utils";
-		home-manager.url = "github:nix-community/home-manager";
+		home-manager.url = "github:nix-community/home-manager/release-23.05";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 	};
 
-	outputs = inputs: {
+outputs = {
+self, home-manager, nixpkgs, flake-utils,nixos-hardware, ...
 
-		homeConfigurations = {
-			homeDirectory = "/home/manolo";
-			username = "manolo";
-			configuration.imports = [ ./home.nix ];
-			stateVersion = "23.05";
-
-		};
-
-	};
+} @inputs:
+let
+    inherit (self) outputs;
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "23.05";
+    libx = import ./lib { inherit inputs outputs stateVersion; };
+  in {
+# Devshell for bootstrapping; acessible via 'nix develop' or 'nix-shell' (legacy)
+    devShells = libx.forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in import ./shell.nix { inherit pkgs; }
+    );
 };
+
+}
 
 
