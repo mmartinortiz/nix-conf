b1206ce68cba0f9464c568f4979ded7095c84182
manolo <mmartinortiz@gmail.com>
2023-07-15 16:12:17 +0200

    Build for every default system

--- flake.nix
+++ flake.nix
@@ -2,18 +2,24 @@
   description = "My Home Manager flake";
 
   inputs = {
+    systems.url = "github:nix-systems/x86_64-linux";
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.cowsay pkgs.alejandra];
-    };
-  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [alejandra];
+      };
+    });
 }
