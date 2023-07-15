fb6ebedd4596badb2ef7e40f47a548757707b966
manolo <mmartinortiz@gmail.com>
2023-07-15 15:25:54 +0200

    First working flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,19 @@
 {
-	description = "My Home Manager flake";
+  description = "My Home Manager flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
-		flake-utils.url = "github:numtide/flake-utils";
-		home-manager.url = "github:nix-community/home-manager/release-23.05";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-outputs = {
-self, home-manager, nixpkgs, flake-utils,nixos-hardware, ...
-
-} @inputs:
-let
-    inherit (self) outputs;
-    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-    stateVersion = "23.05";
-    libx = import ./lib { inherit inputs outputs stateVersion; };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
   in {
-# Devshell for bootstrapping; acessible via 'nix develop' or 'nix-shell' (legacy)
-    devShells = libx.forAllSystems (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in import ./shell.nix { inherit pkgs; }
-    );
-};
-
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [pkgs.cowsay pkgs.alejandra];
+    };
+  };
 }
-
-
