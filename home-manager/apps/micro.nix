158828f889fbd371197a02f67296180aad075199
manolo <mmartinortiz@gmail.com>
2023-07-16 23:21:35 +0200

    Include micro

---
+++ home-manager/apps/micro.nix
@@ -0,0 +1,14 @@
+{
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "material-tc";
+      mkparents = true;
+      softwrap = true;
+      tabmovement = true;
+      tabsize = 2;
+      tabstospaces = true;
+      autosu = true;
+    };
+  };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,6 +3,9 @@
   programs = {
     home-manager.enable = true;
   };
+  imports = [
+    ./apps/micro.nix
+  ];
   home = {
     # Home Manager needs a bit of information about you and the paths it should manage:
     username = "<username>";
