269a65a7e3500fc7c6dde158cdfa975ead2a491d
manolo <mmartinortiz@gmail.com>
2023-09-10 21:09:18 +0200

    Use unstable channel for nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs =
     {
       # systems.url = "github:nix-systems/x86_64-linux";
-      nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       flake-utils = {
         url = "github:numtide/flake-utils";
         # inputs.systems.follows = "systems";
       };
       home-manager =
         {
-          url = "github:nix-community/home-manager/release-23.05";
+          url = "github:nix-community/home-manager";
           inputs.nixpkgs.follows = "nixpkgs";
         };
     };
