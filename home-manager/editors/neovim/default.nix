8debd256febec13902fc50dbc8b50ce31a6e6e2d
manolo <mmartinortiz@gmail.com>
2023-10-01 18:26:20 +0200

    Key mapping for autocompletion

--- home-manager/editors/neovim/default.nix
+++ home-manager/editors/neovim/default.nix
@@ -58,7 +58,38 @@
         snippet.expand = "luasnip";
         mappingPresets = [ "insert" ];
         mapping = {
+          # `Enter` key to confirm completion
           "<CR>" = "cmp.mapping.confirm({ select = true })";
+          # Ctrl+Space to trigger completion menu
+	  "<C-Space>" = "cmp.mapping.complete()";
+          # Navigate between snippet placeholder
+          "<Tab>" = "
+                cmp.mapping(function(fallback)
+                  if cmp.visible() then
+                          cmp.select_next_item()
+                  elseif vim.fn['vsnip#available'](1) == 1 then
+                          feedkey('<Plug>(vsnip-expand-or-jump)', '')
+                  elseif has_words_before() then
+                          cmp.complete()
+                  else
+                          fallback()
+                  end
+                end, {
+                  'i',
+                  's',
+                })";
+
+          "<S-Tab>" = "
+                cmp.mapping(function()
+                  if cmp.visible() then
+                          cmp.select_prev_item()
+                  elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+                          feedkey('<Plug>(vsnip-jump-prev)', '')
+                  end
+                end, {
+                  'i',
+                  's',
+                })";
         };
       };
     };
