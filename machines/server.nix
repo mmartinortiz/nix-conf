0296525ba642227255f1b19b1b5c1c567a65114c
manolo <mmartinortiz@gmail.com>
2023-10-31 20:32:58 +0100

    Replace fish.tmux plugin with a function

--- machines/server.nix
+++ machines/server.nix
@@ -3,19 +3,13 @@
   programs.fish = {
     # Extend fish with the fish.tmux plugin for automatically
     # start and attach to a tmux session on login
-    plugins = [
-      {
-        name = "fish.tmux";
-        src = pkgs.fetchFromGitHub {
-          owner = "budimanjojo";
-          repo = "tmux.fish";
-          rev = "87ef5c238b7fb133d7b49988c7c3fcb097953bd2";
-          sha256 = "sha256-ds1WN10Xlp6BYk1Wooq8NIkVyt5gJguKBH4JBrPo/Qo=";
-        };
-      }
-    ];
     interactiveShellInit = ''
-      set -Ux fish_tmux_config $HOME/.config/tmux/tmux.conf
+    if set -q TMUX
+	    tmux switch -t $session_name
+    else
+	    tmux new-session -D -s "$session_name" -c "$dir" $argv[3..-1] &>/dev/null || \
+		  tmux attach -t "$session_name" -c "$dir"
+    end
     '';
   };
 }
