39fe17cff9c96480041e86c0dabe67c8bc61f366
manolo <mmartinortiz@gmail.com>
2023-11-01 15:53:21 +0100

    Restore fish.tmux plugin

--- machines/server.nix
+++ machines/server.nix
@@ -1,15 +1,19 @@
 {...}: {
   # Server specific configuration
   programs.fish = {
-    # Extend fish with the fish.tmux plugin for automatically
-    # start and attach to a tmux session on login
+    plugins = [
+      {
+        name = "fish.tmux";
+        src = pkgs.fetchFromGitHub {
+          owner = "budimanjojo";
+          repo = "tmux.fish";
+          rev = "87ef5c238b7fb133d7b49988c7c3fcb097953bd2";
+          sha256 = "sha256-ds1WN10Xlp6BYk1Wooq8NIkVyt5gJguKBH4JBrPo/Qo=";
+        };
+      }
+    ];
     interactiveShellInit = ''
-    if set -q TMUX
-      tmux switch -t main
-    else
-      tmux new-session -D -s main -c "$HOME" || \
-      tmux attach -t main -c "$HOME"
-    end
+      set -Ux fish_tmux_config $HOME/.config/tmux/tmux.conf
     '';
   };
 }
