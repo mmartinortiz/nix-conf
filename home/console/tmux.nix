4c7efa6348f0cc15379844118cf384452cba7225
manolo <mmartinortiz@gmail.com>
2023-11-22 09:29:51 +0100

    Reorganize configuration for plugins

--- home/console/tmux.nix
+++ home/console/tmux.nix
@@ -11,7 +11,19 @@
     plugins = with pkgs.tmuxPlugins; [
       nord
       vim-tmux-navigator
-      yank
+      {
+        plugin = yank;
+        extraConfig = ''
+          # Clipboard integration for Linux
+          # bind-key -T copy-mode C-w send-keys -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
+          # bind-key -T copy-mode M-w send-keys -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
+          # bind-key -T copy-mode MouseDragEnd1Pane send -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
+          # bind-key -T root C-y run "xsel -o | tmux load-buffer - ; tmux paste-buffer"
+          set -g @yank_selection_mouse 'clipboard' # or 'primary' or 'secondary'
+          # Get into copy mode with Ctrl-g
+          bind-key -n 'C-g' copy-mode
+        '';
+      }
     ];
     clock24 = true;
     keyMode = "vi";
@@ -19,22 +31,14 @@
     shell = "${config.home.homeDirectory}/.nix-profile/bin/fish";
     terminal = "screen-256color";
     extraConfig = ''
-      # Clipboard integration for Linux
-      # bind-key -T copy-mode C-w send-keys -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
-      # bind-key -T copy-mode M-w send-keys -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
-      # bind-key -T copy-mode MouseDragEnd1Pane send -X copy-pipe-and-cancel "xsel -i -p && xsel -o -p | xsel -i -b"
-      # bind-key -T root C-y run "xsel -o | tmux load-buffer - ; tmux paste-buffer"
-      set -g @yank_selection_mouse 'clipboard' # or 'primary' or 'secondary'
-      # Get into copy mode with Ctrl-g
-      bind-key -n 'C-g' copy-mode
-
-      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded ~/.config/tmux/tmux.conf"
       bind k next-window
       bind j previous-window
 
       # More intuitive vertical and horizontal splits
       bind v split-window -v -c "#{pane_current_path}"
       bind h split-window -h -c "#{pane_current_path}"
+
+      bind r source-file ~/.config/tmux/tmux.conf \; display "Reloaded ~/.config/tmux/tmux.conf"
     '';
   };
 }
