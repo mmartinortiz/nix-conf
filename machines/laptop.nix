a55fe684f9675a4c633ab3f3f866931906e26718
manolo <mmartinortiz@gmail.com>
2023-11-29 10:07:27 +0100

    Abbr for nr on the laptop

--- machines/laptop.nix
+++ machines/laptop.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
   # Laptop specific configuration
+  programs.fish = {
+    shellAbbrs = {
+      nr = pkgs.lib.mkForce "nix run nixpkgs#home-manager -- switch --flake .#laptop";
+    };
+  };
   home.packages = with pkgs; [
     fortune
   ];
