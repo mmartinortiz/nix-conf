19b44830c854643813d757d69242ba46022212b9
manolo <mmartinortiz@gmail.com>
2023-12-03 18:38:49 +0100

    Restructure the default packages

--- home/default.nix
+++ home/default.nix
@@ -1,8 +1,40 @@
-{...}: {
+{pkgs, ...}: let
+  my-packages = with pkgs; [
+    alejandra # Format Nix files
+    chafa # Terminal image viewer
+    deadnix
+    devbox # Create isolated shells for development
+    diffr # Modern Unix `diff`
+    du-dust
+    dua # Modern Unix `du`
+    duf # Modern Unix `df`
+    fd # Modern Unix `find`
+    glow # Terminal Markdown renderer
+    gping # Modern Unix `ping`
+    grc # Colour terminal output
+    procs # Modern Unix `ps`
+    ripgrep # Modern Unix `grep`
+    tldr # Modern Unix `man`
+    wget2
+    (pkgs.nerdfonts.override {fonts = ["Hack" "UbuntuMono" "FiraCode"];})
+  ];
+in {
   home = {
     username = "manolo";
     homeDirectory = "/home/manolo";
     stateVersion = "23.05";
+
+    packages = my-packages;
+
+    # Alternative:
+    # packages =
+    #   my-packages
+    #   ++ stable-packages
+    #   ++
+    #   [
+    #     pkgs.some-package
+    #     pkgs.another-package
+    #   ];
 
     sessionVariables = {
       EDITOR = "nvim";
@@ -12,7 +44,6 @@
   };
 
   imports = [
-    ./console/packages.nix
     ./console/bat.nix
     ./console/bottom.nix
     ./console/direnv.nix
