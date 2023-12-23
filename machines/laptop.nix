6363367fd6bd60a12cc916c2c4c13ea6fde21219
manolo <mmartinortiz@gmail.com>
2023-12-23 11:18:20 +0100

    Local bin path

--- machines/laptop.nix
+++ machines/laptop.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   # Laptop specific configuration
   programs.fish = {
+    interactiveShellInit = ''
+      fish_add_path ~/bin
+    '';
     shellAbbrs = {
       nr = pkgs.lib.mkForce "nix run nixpkgs#home-manager -- switch --flake .#laptop";
     };
