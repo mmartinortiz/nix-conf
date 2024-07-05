3ec8fd9296b366dbd4bed91459e11231c3265f72
manolo <mmartinortiz@gmail.com>
2024-07-05 15:51:43 +0200

    Clean up inputs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,6 @@
   description = "My Home Manager flake";
 
   inputs = {
-    # systems.url = "github:nix-systems/x86_64-linux";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # TODO: Check https://github.com/LGUG2Z/nixos-wsl-starter/blob/master/flake.nix
-    # for installing packages both from stable an unstable.
-    # Requires an overlay
-    # flake-utils = {
-    #   url = "github:numtide/flake-utils";
-    #   # inputs.systems.follows = "systems";
-    # };
-    # home-manager = {
-    #   url = "github:nix-community/home-manager";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
     kickstart-nix-nvim = {
       url = "github:mmartinortiz/neovim-conf";
     };
