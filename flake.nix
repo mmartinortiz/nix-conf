9029174e3e5d55130bb5378d4d4b61a396e0470b
manolo <mmartinortiz@gmail.com>
2024-11-06 22:00:12 +0100

    New home config

--- flake.nix
+++ flake.nix
@@ -1,46 +1,32 @@
 {
-  description = "My Home Manager flake";
+  description = "Home Manager configuration of manolo";
 
   inputs = {
-    kickstart-nix-nvim = {
-      url = "github:mmartinortiz/neovim-conf";
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
     nixpkgs,
     home-manager,
-    kickstart-nix-nvim,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    homeConfigurations = {
-      laptop = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          {
-            nixpkgs.overlays = [
-              kickstart-nix-nvim.overlays.default
-            ];
-          }
-          ./home/default.nix
-          ./machines/laptop.nix
-        ];
-      };
-      server = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          {
-            nixpkgs.overlays = [
-              kickstart-nix-nvim.overlays.default
-            ];
-          }
-          ./home/default.nix
-          ./machines/server.nix
-        ];
-      };
+    homeConfigurations."manolo" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [./home.nix];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
     };
   };
 }
