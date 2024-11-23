465ca056cd763c094b30b778eeddc15440743f3c
manolo <mmartinortiz@gmail.com>
2024-11-23 12:16:49 +0100

    Remove home definition

--- common.nix
+++ common.nix
@@ -4,20 +4,6 @@
   lib,
   ...
 }: {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "manolo";
-  home.homeDirectory = "/var/home/manolo";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
   imports = [
     ./modules/bat.nix
     ./modules/bottom.nix
